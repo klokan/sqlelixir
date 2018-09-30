4144dd835aa11501161b987412b9e9b28210bda9
Václav Klusák <vaclav.klusak@klokantech.com>
2018-09-30 15:49:04 +0200

    Release 1.11.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='SQLElixir',
-    version='1.10',
+    version='1.11',
     description='SQL files as Python modules',
     packages=['sqlelixir'],
     install_requires=[
