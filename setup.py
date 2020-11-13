74ea088921a635408e2bc389a4e25d3488668f9d
Václav Klusák <vaclav.klusak@maptiler.com>
2020-11-13 18:03:11 +0100

    Release 1.19

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='SQLElixir',
-    version='1.18',
-    description='SQL files as Python modules',
-    packages=['sqlelixir'],
+    name="SQLElixir",
+    version="1.19",
+    description="SQL files as Python modules",
+    packages=["sqlelixir"],
     install_requires=[
-        'sqlalchemy>=1.1.0',
-        'sqlparse>=0.2',
+        "sqlalchemy>=1.1.0",
+        "sqlparse>=0.2",
     ])
