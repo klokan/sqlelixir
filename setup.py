96b85ce9e7ab1110a021f72ac95d2bed0fc8cd3f
Václav Klusák <vaclav.klusak@maptiler.com>
2021-01-19 10:50:42 +0100

    Release 1.21

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="SQLElixir",
-    version="1.20",
+    version="1.21",
     description="SQL files as Python modules",
     packages=["sqlelixir"],
     install_requires=[
