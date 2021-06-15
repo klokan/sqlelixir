d12f852775103b55608e5fa81b34d4fa29f1a1f2
Václav Klusák <vaclav.klusak@klokantech.com>
2021-06-15 13:58:47 +0200

    Release 1.22

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name="SQLElixir",
-    version="1.21",
+    version="1.22",
     description="SQL files as Python modules",
     packages=["sqlelixir"],
     install_requires=[
-        "sqlalchemy>=1.1.0",
+        "sqlalchemy>=1.4.0",
         "sqlparse>=0.2",
     ])
