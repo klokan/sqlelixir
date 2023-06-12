89ec6bd37786baade2e3479a2ce840bdafcae312
Václav Klusák <vaclav.klusak@klokantech.com>
2023-06-12 08:00:17 +0000

    Release 2.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="SQLElixir",
-    version="2.12",
+    version="2.13",
     description="SQL files as Python modules",
     packages=["sqlelixir"],
     install_requires=[
